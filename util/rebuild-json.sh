b49d95db1e7d7b96c352a86570bdea121447d5c1
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-11-15 21:04:21 -0600

    likewise fix for py3

--- util/rebuild-json.sh
+++ util/rebuild-json.sh
@@ -1,4 +1,4 @@
 #
 Echo "rebuilding Json's from yaml files"
-./sabayon-packer/util/make.py ./sabayon-packer/yaml/*.json > ./sabayon-packer/
+python3 ./sabayon-packer/util/make.py ./sabayon-packer/yaml/*.json > ./sabayon-packer/
 Echo "rebuilding Compleaded"
